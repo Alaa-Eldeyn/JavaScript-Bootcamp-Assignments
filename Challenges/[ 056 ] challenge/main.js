fc691ac24335bed9ed159bc85a6aea9116bf5b11
Alaa Eldeyn <93477209+Alaa-Eldeyn@users.noreply.github.com>
2023-01-31 00:56:27 +0200

    Update main.js

--- Challenges/[ 056 ] challenge/main.js
+++ Challenges/[ 056 ] challenge/main.js
@@ -1,3 +1,22 @@
-let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
+let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia", "Anwar"];
+let showCount = 3
 
-// Elzero
+document.write(`<div>We Have <span>${showCount}</span> Admins</div>`);
+document.write(`<hr>`);
+
+for (i = 0; i <= showCount; i++){
+    if (myAdmins[i] === "Stop" || myAdmins[i] === "stop") break;
+    document.write(`<div>`);
+    document.write(`<p><strong>The Admin For Team <span>${i + 1}</span> is ${myAdmins[i]}</strong></p>`);
+    document.write(`<h3>Team members:</h3>`);
+    let counter = 1;
+    for (j = 0; j < myEmployees.length; j++){
+        if (myEmployees[j][0] == myAdmins[i][0]) {
+            document.write(`<p>${counter++}- ${myEmployees[j]}</p>`)
+        }
+    }
+    counter = 1
+    document.write(`</div>`);
+    document.write(`<hr>`);
+}
